fd3d149e867c908c15e86a3f9daead8f45b433c3
Ahmad Zidni Hidayat <zdngaming312@gmail.com>
2025-02-16 22:19:53 +0700

    feat: add HomeFeedSkeleton component for improved loading state in HomeSongsSection

--- frontend-nextjs/modules/home/ui/sections/HomeSongsSection.tsx
+++ frontend-nextjs/modules/home/ui/sections/HomeSongsSection.tsx
@@ -1,10 +1,14 @@
 "use client";
-import { SongGridCard } from "@/modules/songs/ui/components/SongGridCard";
-import { useSuspenseInfiniteQuery } from "@tanstack/react-query";
+
 import { Suspense } from "react";
 import { ErrorBoundary } from "react-error-boundary";
-import { recommendationsInfiniteQueryOptions } from "../../api/recommendationsInfiniteQueryOptions";
+import { useSuspenseInfiniteQuery } from "@tanstack/react-query";
+
 import { InfiniteScroll } from "@/components/InfiniteScroll";
+import { SongGridCard } from "@/modules/songs/ui/components/SongGridCard";
+
+import { recommendationsInfiniteQueryOptions } from "../../api/recommendationsInfiniteQueryOptions";
+import { HomeFeedSkeleton } from "@/modules/songs/ui/skeletons/HomeFeedSkeleton";
 
 interface HomeSongsSectionProps {
   categoryId?: string;
@@ -12,22 +16,13 @@ interface HomeSongsSectionProps {
 
 export const HomeSongsSection = ({ categoryId }: HomeSongsSectionProps) => {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
+    <Suspense fallback={<HomeFeedSkeleton />}>
       <ErrorBoundary fallback={<div>Something went wrong</div>}>
         <HomeSongsSectionSuspense categoryId={categoryId} />
       </ErrorBoundary>
     </Suspense>
   );
 };
-
-// const data2 = {
-//   id: 1,
-//   title: "Title",
-//   views: 1276000,
-//   createdAt: new Date().toISOString(),
-//   user_id: 1,
-// };
-
 const HomeSongsSectionSuspense = ({ categoryId }: HomeSongsSectionProps) => {
   const { data, hasNextPage, fetchNextPage, isFetchingNextPage } = useSuspenseInfiniteQuery({
     ...recommendationsInfiniteQueryOptions,
@@ -36,19 +31,10 @@ const HomeSongsSectionSuspense = ({ categoryId }: HomeSongsSectionProps) => {
       return lastPage.pagination.next_cursor;
     },
   });
-
   const data2 = data.pages.flatMap((page: any) => page.data);
-  // TODO: implement InfiniteScroll
   return (
     <div>
       <div className="gap-4  gap-y-10 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-4">
-        {/* {Array.from({ length: 10 }).map((_, index) => (
-          <SongGridCard
-            key={index}
-            data={data2}
-          />
-        ))} */}
-
         {data2.map((song: any, index: number) => (
           <SongGridCard
             key={index}
